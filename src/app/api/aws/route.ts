eeca6dd5b832ef51d9da26afc383dd107753c6e5
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-08-25 19:45:59 -0300

    :sparkles: gerando numero pedido

--- src/app/api/aws/route.ts
+++ src/app/api/aws/route.ts
@@ -1,38 +1,41 @@
 const url = `${process.env.AWS_LAMBDA_URL_CRIAR_PEDIDO}`;
 
-export async function POST() {
+export async function POST(req: Request) {
+  const data = await req.json();
+
+  console.log(data);
 
   const response = await fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      pedido: "VIM DO MagaluCloud",
+      pedido: data.pedido,
       item: "CARNE",
-      status: [
-        {
-          id: 1,
-          descricao: "Pedido criado",
-          data: "22/08/2024"
-        }
-      ]
+      status: {
+        id: 1,
+        descricao: "Pedido criado",
+        data: "22/08/2024",
+      },
     }),
   });
 
   if (response.status === 201) {
-    return Response.json({
-      "mensagem": "Enviado para SNS com sucesso"
-    }, {
-      status: 201
-    });
+    return Response.json(
+      {
+        mensagem: "Enviado para SNS com sucesso",
+      },
+      {
+        status: 201,
+      }
+    );
   } else {
     return Response.json({
       statusCode: 500,
       body: JSON.stringify({
-        message: 'Falha ao tentar criar o pedido no SNS',
+        message: "Falha ao tentar criar o pedido no SNS",
       }),
     });
   }
-
 }
--- src/components/restaurante/criarPedido.tsx
+++ src/components/restaurante/criarPedido.tsx
@@ -4,22 +4,45 @@ import { useState } from "react";
 import { Button } from "../ui/button";
 
 export default function CriarPedido() {
-  const [mensagem, setMensagem] = useState("");
+  const [pedido, setPedido] = useState("");
+
+  function generateOrderNumber(): string {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let orderNumber = "";
+
+    for (let i = 0; i < 5; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      orderNumber += characters[randomIndex];
+    }
+
+    return orderNumber;
+  }
 
   const criarPedido = async () => {
+    const pedidoGerado = generateOrderNumber();
+
     fetch("/api/aws/", {
       method: "POST",
+      body: JSON.stringify({ pedido: pedidoGerado }),
       headers: { "Content-Type": "application/json" },
     });
-    setMensagem("Pedido criado com sucesso! - " + new Date().toISOString());
+
+    setPedido(pedidoGerado);
   };
 
   return (
     <div className="text-center">
       <div>
-        <Button onClick={() => criarPedido()}>Teste SQS</Button>
+        <Button disabled={!!pedido} onClick={() => criarPedido()}>
+          Fazer meu pedido
+        </Button>
       </div>
-      <div>{mensagem}</div>
+      {pedido && (
+        <div className="mt-8">
+          <div>Pedido criado com o número: {pedido}</div>
+          <div>Aguarde um momento enquanto o pedido é processado.</div>
+        </div>
+      )}
     </div>
   );
 }
