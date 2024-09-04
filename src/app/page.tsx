6e76c6c91f19af46b4a3f1a7ba5ad1ca0401aee2
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-09-04 13:44:29 -0300

    :lipstick: re restilando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,19 @@ import CriarPedido from "@/components/restaurante/criarPedido";
 
 export default function Home() {
   return (
-    <div>
-      <div className="text-center text-3xl my-8">
-        <h1>Restaurante Tempeiro & Amor</h1>
-      </div>
+    <main className="max-w-sm mx-auto mt-3">
+      <div className="border-2 border-red-600 m-2 p-2 rounded-xl pb-8">
+        <div className="text-3xl my-8 flex justify-center text-center space-x-1">
+          <div>
+            <h1 className="font-light italic">Restaurante </h1>
+            <h2 className="text-red-700 font-mono font-semibold mt-2">Tempeiro & Amor</h2>
+          </div>
+        </div>
 
-      <div className="max-w-lg mx-auto flex flex-col items-center justify-center space-y-3">
-        <div>Estamos em construção</div>
-
-        <CriarPedido />
+        <div className="">
+          <CriarPedido />
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
